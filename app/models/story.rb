d9590ca0f2c50cdef94517bfc62932166b17f92e
yuyurun <yuyu1.run@icloud.com>
2017-08-03 05:49:12 +0000

    name should not be too long

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,3 @@
 class Story < ApplicationRecord
+  validates :name, presence: true, length: { maximum:20 }
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -4,4 +4,14 @@ class StoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @story = Story.new(name: "Example",content: "Example content")
+  end
+
+  test "name should not be too long" do
+    @story.name = "a"*21
+    assert_not @story.valid?
+  end 
+
 end
