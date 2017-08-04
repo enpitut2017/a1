ba546f0b87bd92929d63ab50be9344316086b442
“NaOboe” <k.nao0626@gmail.com>
2017-08-04 06:46:40 +0000

    大宮よろしく

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804004652) do
+ActiveRecord::Schema.define(version: 20170804051031) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170804004652) do
     t.integer "category_id"
     t.string "company"
     t.integer "department_id"
+    t.integer "year"
     t.index ["department_id"], name: "index_stories_on_department_id"
   end
 
