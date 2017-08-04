feec99d9e705b7906f52534bd8bd7affe2fcfef2
kato11433 <s1511433@u.tsukuba.ac.jp>
2017-08-04 05:43:01 +0000

    db/seeds.rbを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+Department.create(:name => '人文学類')
+Department.create(:name => '比較文化学類')
+Department.create(:name => '日本語・日本文化学類')
+Department.create(:name => '社会学類')
+Department.create(:name => '国際総合学類')
+Department.create(:name => '教育学類')
+Department.create(:name => '心理学類')
+Department.create(:name => '障害科学類')
+Department.create(:name => '生物学類')
+Department.create(:name => '生物資源学類')
+Department.create(:name => '地球学類')
+Department.create(:name => '数学類')
+Department.create(:name => '物理学類')
+Department.create(:name => '化学類')
+Department.create(:name => '応用理工学類')
+Department.create(:name => '工学システム学類')
+Department.create(:name => '社会工学類')
+Department.create(:name => '情報科学類')
+Department.create(:name => '情報メディア創成学類')
+Department.create(:name => '知識情報・図書館学類')
+Department.create(:name => '医学類')
+Department.create(:name => '看護学類')
+Department.create(:name => '医療科学類')
+Department.create(:name => '体育専門学群')
+Department.create(:name => '芸術専門学群')
+
+Story.create(:name => '小島', :content => '面接に落ちた', :category_id => '1', :year => '1996', :company => 'SONY', :department_id => '2')
