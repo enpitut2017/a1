d449849216bbea37517866d2d5e43cd611b900e4
Kousuke <sasaki-trombone@users.noreply.github.com>
2017-08-07 10:51:43 +0900

    いいねを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807013436) do
+ActiveRecord::Schema.define(version: 20170807014911) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170807013436) do
     t.integer "year"
     t.integer "month"
     t.text "title"
+    t.integer "like_count"
     t.index ["department_id"], name: "index_stories_on_department_id"
   end
 
