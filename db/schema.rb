e3dde7778e0ec19e48971e45228a1799498cc550
Kousuke <sasaki-trombone@users.noreply.github.com>
2017-08-08 10:17:55 +0900

    migration fileを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807014911) do
+ActiveRecord::Schema.define(version: 20170808011215) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170807014911) do
     t.integer "year"
     t.integer "month"
     t.text "title"
-    t.integer "like_count", null: false, default: 0
+    t.integer "like_count", default: 0, null: false
     t.index ["department_id"], name: "index_stories_on_department_id"
   end
 
