8c5c337bdb86d534fd9add2a03be3ea186c121d3
Kousuke <sasaki-trombone@users.noreply.github.com>
2017-08-08 11:08:10 +0900

    migration追記

--- db/migrate/20170807014911_add_like_to_story.rb
+++ db/migrate/20170807014911_add_like_to_story.rb
@@ -1,5 +1,5 @@
 class AddLikeToStory < ActiveRecord::Migration[5.1]
   def change
-    add_column :stories, :like_count, :integer
+    add_column :stories, :like_count, :integer, null: false, default: 0
   end
 end
